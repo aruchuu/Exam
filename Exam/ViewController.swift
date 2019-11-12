c2ed6f75be81240b92a52d3daa835568855cf24d
miyamoto suzune <sz38moto.bt@gmail.com>
2019-11-12 19:16:12 +0900

    Initial Commit

---
+++ Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam
+//
+//  Created by 宮本涼音 on 2019/11/12.
+//  Copyright © 2019 宮本涼音. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
